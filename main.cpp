868174fda68c30970a3f8d0ed1984f71bc7c922d
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-08-04 20:05:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 #include "utils.hpp"
 #include "ltype.hpp"
+#include "astNode.hpp"
+#include "symbolTable.hpp"
 
 using namespace std;
 
@@ -16,52 +18,8 @@ class symbolTable;
 
 
 
-class symbolTable{
-  public:
-  map<string,lType*>& tbl;
-  symbolTable* parent;
-  bool p = false;
-  symbolTable(map<string,lType*>& t):tbl(t){
-    
-  }
-  void setParent(symbolTable* par){
-    parent = move(par);
-    p = true;
-  }
-  lType* get(string nm){
-    if(tbl.find(nm) == tbl.end()){
-      if (p){ 
-        return parent->get(nm);
-      }
-      else{
-        error("name not found");
-        return nullptr;
-      }
-    }
-    else {
-      return move(tbl[nm]);
-    }
-  }
-  void set(string n,lType* v){
-    tbl[n] = move(v);
-  }
-};
 
 
-
-class astNode{
-  public:
-  virtual bool isRet(){
-    return false;
-  }
-  virtual lType* exec( symbolTable st ){
-    cout<<":("<<endl;
-    error("...");
-  }
-  virtual void print(){
-    cout << ":(p"<<endl;
-  }
-};
 
 
 class numberType: public lType{
