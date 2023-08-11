f5b8724e4df53f3c3b5b389bc54da136c23d0be6
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-08-11 17:59:12 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #define INFO 1
 
 #include <iostream>
-#include <map>
-#include <vector>
+#include <map>//
+#include <vector>////
 #include <functional>
 #include <fstream>
 #include <sstream> 
 #include <regex>
+#include <filesystem>
+#include <tuple>
 
 
 using namespace std;
@@ -22,16 +24,18 @@ class symbolTable;
 #include "symbolTable.hpp"
 #include "types/lType_base.hpp"
 #include "ast/astNode_base.hpp"
+
 #include "types/lType.hpp"
 #include "ast/astNode.hpp"
-
 
 #include "parser/token.hpp"
 #include "parser/lexer.hpp"
 #include "parser/parser.hpp"
 
-
 #include "preprocess.hpp"
+#include "getMod.hpp"
+
+
 
 
 
@@ -42,27 +46,31 @@ lType* print(vector<lType*> inp){
   return new None;
 }
 
-///
-int main(int argc ,char** argv) {///////////
+
+int main(int argc ,char** argv) {
+  vector<string> b;
   map<string,lType*> m;
   symbolTable gst(m);
   builtInFn printFN(1,&print);
   gst.set("print", &printFN);
   string inp = readFile("main.?");
-  cout << "!" << endl;
-  inp = imp(inp);
+  auto tp = imp(inp);
+  inp = get<0>(tp);
+  b = get<1>(tp);
+  for (string mn: b){
+    getMod(mn,gst);
+  };
   lexer ls (inp);
-  cout << '!'<<endl;
   vector<token>r = ls.gen_toks();
   if (INFO){  
     for(auto e:r) e.print();
     cout<<endl;
-  }//
+  }
   parser p (r);
   astNode* exp = p.block();
   if (INFO){ exp -> print(); }
   cout<<endl;
   exp -> exec(gst) -> print();
   cout<<endl;
-    ///
+    
 }
