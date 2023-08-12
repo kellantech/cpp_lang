91f40d27f7700057b299e37bcae197ac740aa182
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-08-12 16:00:46 -0400

    Update math.hpp

--- stdlib/math.hpp
+++ stdlib/math.hpp
@@ -22,10 +22,40 @@ lType* lsin(vector<lType*> v){
   return new numberType(rs);
 }
 
+lType* lcos(vector<lType*> v){
+  lType* n = v[0];
+  lType* c = ((new numberType(1.5707))->sub(n));
+  return lsin(vector<lType*>{c});
+}
+
+lType* lexp(vector<lType*> v){
+  lType* n = v[0];
+  return new numberType(pow(2.71828,n->iget()));
+}
+
+lType* lsqrt(vector<lType*> v){
+  lType* n = v[0];
+  return new numberType(pow(n->iget(),0.5));
+}
+lType* lhyp(vector<lType*> v){
+  lType* a = v[0];
+  lType* b = v[1];
+  return new numberType(pow(pow(a->iget(),2)+pow(b->iget(),2),0.5));
+}
+
+
+
 
 
 void MATH_add(symbolTable& st){
   
   st.set("fact", new builtInFn(1,&fact));
   st.set("sin", new builtInFn(1,&lsin));
+  st.set("cos", new builtInFn(1,&lcos));
+  st.set("exp", new builtInFn(1,&lexp));
+  st.set("sqrt", new builtInFn(1,&lsqrt));
+  st.set("hyp", new builtInFn(2,&lhyp));
+  
+  st.set("pi", new numberType(3.14159));
+  st.set("e", new numberType(2.71828));
 }
