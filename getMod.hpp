1c4b79cc29b1e7b09f962e0a9475437a66ae8007
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-08-13 13:36:31 -0400

    Update getMod.hpp

--- getMod.hpp
+++ getMod.hpp
@@ -1,9 +1,14 @@
 #include "stdlib/math.hpp"
+#include "stdlib/rand.hpp"
+
 
 
 void getMod(string nm,symbolTable& st){
   if (nm == "math"){
     MATH_add(st);
+  }
+  else if (nm == "random"){
+    RAND_add(st);
   }
   else {
     error("no module named "+nm);
